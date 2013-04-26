516a0f245d2443afa89723577cf59e23219a0b62
marcio <mgalli@telasocial.com>
2013-04-25 21:05:49 -0300

    Path

--- scripts/screenshots/index.sh
+++ scripts/screenshots/index.sh
@@ -1,6 +1,7 @@
 #!/bin/sh 
 echo 'screenshot taking'
 export DISPLAY=:0.0
+cd /home/tela/TelaSocial-Mediator
 /usr/bin/xwd -root -out file.xwd
 convert file.xwd current.jpg
 cp current.jpg ./app/static
