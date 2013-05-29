4e4320e4f116fc4633b40ca8193f0f6fa1fe2b9b
marcio <mgalli@telasocial.com>
2013-05-29 11:40:38 -0300

    Clock cleanup

--- static/tempo/clock.js
+++ static/tempo/clock.js
@@ -35,10 +35,9 @@
  *
  * ***** END LICENSE BLOCK ***** */
 
-var tempoWidget =  {
+var app =  {
 	feed: null, 
 	elementClock  : null, 
-        mos: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
 	start : function () { 
 		var mainDiv = document.createElement('div');
 		document.body.appendChild(mainDiv);
@@ -46,26 +45,11 @@ var tempoWidget =  {
 		this.elementClock.setAttribute("id","clock");
 		this.elementClock.setAttribute("class","datetime");
 		mainDiv.appendChild(this.elementClock);
-		this.feed = $;
-		var self = this;
 		this.tick();
-
 	} ,
 
-	updateFeed : function() {
-		var self = this;
-	},
-
-	__feedUpdated : function(xml) {
-
-		var self  = this; 
-		var content = self.feed(xml).find('description').text();
-		setTimeout( function(){self.updateFeed()},  1000);
-	},
 	tick : function () {
 		this.data = new Date();
-		//var text = this.data.toLocaleTimeString();
-		//var text = this.data.toTimeString();
 		var hour = this.data.getHours();
 		var min = this.data.getMinutes();
 		var secs = this.data.getSeconds();
@@ -78,16 +62,9 @@ var tempoWidget =  {
 			prefixmin = "0";
 		} 
 
-		//text = text.replace(/:..( [AP]M)$/, '$1');
 		this.elementClock.innerHTML = hour+":"+prefixmin+min+":"+prefix+secs;
 		var scopedThis = this;
 		setTimeout( function () { scopedThis.tick() }, 1000);
-
-		var ddd = new Date();
-		var yy = ddd.getFullYear();
-		var dd = ddd.getDate();
-		var mm = this.mos[ddd.getMonth()];
-
 	}
 }
 
